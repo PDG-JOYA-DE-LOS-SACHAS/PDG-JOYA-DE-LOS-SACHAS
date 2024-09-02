ecafb99f9cc30fcc9448b46e4b7f5ba842a70510
PDG-JOYA-DE-LOS-SACHAS <pdea.orei013@gmail.com>
2024-09-02 14:53:09 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include('config.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre_usuario = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id, password FROM empleados WHERE nombre_usuario = '$nombre_usuario'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['employee_id'] = $row['id'];
+            header("Location: dashboard.html");
+        } else {
+            echo "Contraseña incorrecta.";
+        }
+    } else {
+        echo "Nombre de usuario incorrecto.";
+    }
+}
+
+$conn->close();
+?>
